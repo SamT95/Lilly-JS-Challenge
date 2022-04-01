046038487d266db63975fb8ac09aa9c29c59516e
SamT95 <samuelthompson99@outlook.com>
2022-04-01 12:18:36 +0100

    First commit

--- app.js
+++ app.js
@@ -10,12 +10,36 @@ app.get('/stocks', async (req, res) => {
   res.send({ stockSymbols })
 })
 
+
 app.get('/stocks/:symbol', async (req, res) => {
-  const { params: { symbol } } = req
-  const data = await stocks.getStockPoints(symbol, new Date())
-  res.send(data)
-  console.log(`Recent stock price for ${symbol}: `)
-  console.table(data)
+  try {
+    const { params: { symbol } } = req
+    const data = await stocks.getStockPoints(symbol, new Date())
+    res.send(data)
+    console.log(`Recent stock price for ${symbol}: `)
+    console.table(data)
+  }catch (error) {
+    console.log(error) //Maybe encase whole process in fuction and recall?
+  }
 })
+
+
+/*
+app.get('/stocks/:symbol', async (req, res) => {
+  const getStockData = async function() {
+      try {
+        const { params: { symbol } } = req
+        const data = await stocks.getStockPoints(symbol, new Date())
+        res.send(data)
+        console.log(`Recent stock price for ${symbol}: `)
+        console.table(data)
+      }catch (error) {
+        console.log(error) //Maybe encase whole process in fuction and recall?
+        getStockData()
+      }
+  }
+})
+*/
+
 
 app.listen(3000, () => console.log('Server is running!'))
